d525a7bd7f7714c21a1cfb9ae0fbe459a7f17e56
marcinhojazz <marcinhojazz@gmail.com>
2022-06-01 16:20:57 -0300

    update site place material texture

--- components/Box/index.jsx
+++ components/Box/index.jsx
@@ -1,10 +1,15 @@
 import React from "react"
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 function Box(props) {
+  const texture = useLoader(TextureLoader, "/texture.jpg");
+
   return (
     <mesh {...props} receiveShadow={true} castShadow={true}>
       <boxBufferGeometry />
-      <meshPhysicalMaterial color="white" />
+      <meshPhysicalMaterial map={texture} color={"white"} />
+
     </mesh>
   )
 }
