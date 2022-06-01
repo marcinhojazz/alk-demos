94828099b325a2c4acb9b93fb44798646c24aea7
marcinhojazz <marcinhojazz@gmail.com>
2022-06-01 10:46:19 -0300

    correction blank start project

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,6 @@ import OrbitControls from "../components/OrbitControls";
 import LightBulb from '../components/LightBulb'
 import styles from '../styles/Home.module.css'
 import Draggable from '../components/Draggable';
-import Arial from ""
-
-
-
-function Text3d(){
-   const font = new THREE.FontLoader().parse(Arial);
-   const textOptions = {
-      font,
-      size: 5,
-      height: 1
-   };
-   return (
-      <mesh>
-         <textGeometry attach='geometry' args={['three.js', text Options]} />
-         <meshStandardMaterial attach='material' color="hotpink" />
-       </mesh>
-    )
-}
 
 export default function Home() {
   return (
@@ -37,8 +19,6 @@ export default function Home() {
       >
           <ambientLight color={"white"} intensity={0.2} />
           <LightBulb position={[0, 3, 0]} />
-          
-          <Text3d />
           
           <Draggable>
             <Box rotateX={3} rotateY={0.2} />
